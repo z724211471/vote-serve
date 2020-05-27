35bfcfe9d3ef55c213ad161c15c3a98df024c310
724211471 <724211471@qq.com>
2020-05-27 17:58:30 +0800

    first commit

---
+++ vote/src/main/java/com/example/vote/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
